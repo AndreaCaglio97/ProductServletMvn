f2418678c76a8481b2826be3f0c2456487e78ba7
Caglio Andrea <caglio@intre.it>
2017-11-20 11:38:55 +0100

    eliminato vecchio progetto

--- ProductServletMvn/src/main/java/it/intre/ProductServletMvn/utilities/Rounding.java
+++
@@ -1,8 +0,0 @@
-package it.intre.ProductServletMvn.utilities;
-
-public class Rounding {
-    public static double roundingUpForExcess5Cents(double number)
-    {
-        return Math.ceil(number * 20) / 20;
-    }
-}
